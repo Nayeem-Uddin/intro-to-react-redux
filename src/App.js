b2c455d4940982ecaf023fb93a2e34ab091fb289
Nayeem-uddin <nayeemu02@gmail.com>
2021-03-19 01:29:31 +0600

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement} from './Actions'
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React-Reducer</h1>
+      <h4>Counter : {counter}</h4>
+      <button onClick={()=> dispatch(increment(5))}>increment</button>
+      <br/><br/>
+      <button onClick={()=> dispatch(decrement())}>decrement</button>
+      { isLogged ? <h3>You cann't see the valuable information</h3> : <h3>U can see the information</h3>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import allReducer from './Reducers'
+
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+//STORE -> Globalized all the states
+
+//Action -> increment -> What actually we will do here
+// const Increment = () => {
+//   return {
+//     type:'INCREMENT'
+//   }
+// } 
+
+// const Decrement = () =>{
+//   return {
+//     type:'DECREMENT'
+//   }
+// }
+// //Reducer -> checks how out action changes/transform from one state to the new state
+
+// const Counter = (count = 0, action) =>{
+//   switch (action.type){
+//     case 'INCREMENT':
+//       return count + 1
+//     case 'DECREMENT':
+//       return count - 1
+//   }
+
+// }
+// let store = createStore(Counter)
+
+// //display it on the console
+// store.subscribe(()=> console.log(store.getState()))
+
+// //Dispatch -> dispatch the action with reducer and update it in the store
+// store.dispatch(Increment())
+// store.dispatch(Decrement())
+// store.dispatch(Decrement())
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+         <App />
+    </Provider>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
